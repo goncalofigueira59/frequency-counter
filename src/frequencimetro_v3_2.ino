540d838812b3d33ef2e0c861fd1d6df16f9ebcc8
goncalofigueira59 <2tn9zmdjxk@privaterelay.appleid.com>
2026-02-06 22:38:31 +0000

    feat: código fonte do frequencímetro v3.2

---
+++ src/frequencimetro_v3_2.ino
@@ -0,0 +1,117 @@
+/*
+           ________________________________________________________
+          |  ____________________________________________________  |
+          | |                                                    | |
+          | |         Projeto EMI 1º ano - 1º semestre           | |
+          | |               Frequencímetro V3.2                  | |
+          | |                                                    | |
+          | | Autores:                                           | |
+          | | - Aluno 1, 115216;                                 | |
+          | | - Gonçalo Figueira, 109438;                        | |
+          | | - Aluno 2, 118734;                                 | |
+          | | - Aluno 3, 115154;                                 | |
+          | | - Aluno 4, 119337;                                 | |
+          | |                                                    | |
+          | |          Placa utilizada: Arduino Uno R3;          | |
+          | |____________________________________________________| |
+          |________________________________________________________|
+
+*/
+
+#include <LiquidCrystal_I2C.h>  // Biblioteca do Display com o módulo I2C
+
+LiquidCrystal_I2C lcd(39, 16, 2);  // Configuração do display
+
+int pinosensor = A0;           // Pino A0 que vai fazer a leitura da tensão
+const byte interruptPin = 2;  // Pino 2 que vai fazer a leitura da frequência
+
+float contador = 0;              // Contador de interrupções
+long tempofrequenciafinal = 0;  // Armazena o tempo da última medição
+
+struct VARIAVEIS_FREQUENCIA {
+
+  unsigned int frequencia = 0;  // Frequência calculada
+};
+
+struct VARIAVEIS_TENSAO {
+
+  int valorinstantanea[625];    // Array para armazenar leituras da tensão
+  int maiorvalor;              //Maior valor de tensao medida pelo o arduino
+  float tensaopico;           // Tensão de pico
+  float tensaorms;           // Tensão RMS
+};
+
+void ONDA0V() {
+
+  contador++;  // Contador que incrementa a cada interrupção
+}
+
+// Função para calcular frequência
+void calcularFrequencia() {
+
+  VARIAVEIS_FREQUENCIA frequenciamedida;
+
+  frequenciamedida.frequencia = contador / 2;  // Calcula a frequência
+  contador = 0;                               // Reinicia o contador
+
+  lcd.setCursor(5, 1);                              //Posicionamento do Cursor
+  lcd.print((String)frequenciamedida.frequencia);  //Imprime o valor da frequencia no LCD
+  lcd.print("Hz ");                               //Imprime as unidades “Hz” no LCD
+}
+
+// Função para calcular tensão
+void calcularTensao() {
+
+  VARIAVEIS_TENSAO tensao;
+  for (int i = 0; i < 625; i++) {
+
+    tensao.valorinstantanea[i] = analogRead(pinosensor);  //Faz leituras da tensão
+
+    delay(2);  // Espera 2 milissegundos
+  }
+
+  for (int i = 0; i < 625; i++) {
+
+    tensao.maiorvalor = max(tensao.maiorvalor, tensao.valorinstantanea[i]);  // Identifica o maior valor lido
+  }
+}
+
+tensao.tensaopico = ((tensao.maiorvalor * 10) / 1024) * 1.11;   //Calcula a tensão de pico
+tensao.tensaorms = tensao.tensaopico / 1.4;                    //Calcula a tensão RMS
+tensao.maiorvalor = 0;                                        // Reinicia a variável do maior valor
+
+lcd.setCursor(5, 0);                   //Posiciona o cursor no LCD
+lcd.print((String)tensao.tensaorms);  //Imprime o valor da tensao RMS
+lcd.print("V ");                     //Imprime as unidades de voltagem, V
+}
+
+void setup() {
+
+  Serial.begin(9600);
+
+  lcd.setBacklight(HIGH);
+  lcd.init();
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Vrms:");
+  lcd.setCursor(0, 1);
+  lcd.print("Freq:");
+
+  pinMode(pinosensor, INPUT);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), ONDA0V, CHANGE);  // Configuração da interrupção
+}
+
+void loop() {
+
+  if (millis() - tempofrequenciafinal >= 1000) {
+
+    detachInterrupt(digitalPinToInterrupt(interruptPin));  // Temporariamente desativa a interrupção para nao interferir nas leituras
+
+    calcularFrequencia();  // Chama a função para calcular a frequência
+    calcularTensao();     // Chama a função para calcular a tensão
+
+    attachInterrupt(digitalPinToInterrupt(interruptPin), ONDA0V, CHANGE);  // Reativa a interrupção
+
+    tempofrequenciafinal = millis();  // Atualiza o tempo da última medição
+  }
+}
